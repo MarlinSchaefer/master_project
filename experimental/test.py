988b0c79c16f67f1fabee173ff33c58f338fddc9
Marlin Schäfer <mb.schaefer@gmx.de>
2019-01-08 15:50:39 +0100

    Modified test-file to test new functionality.

--- experimental/test.py
+++ experimental/test.py
@@ -1,4 +1,5 @@
 from run_net import run_net
+import numpy as np
 
 if __name__ == "__main__":
-    run_net('test_net', 'test', num_of_templates=20, mass1=[10.0,45.0], overwrite_template_file=False, ignore_fixable_errors=True)
+    run_net('varied_nothing_net_v2', 'test', epochs=10, coa_phase=[0.0, np.pi],random_starting_time=True, snr=[5.0,12.0], num_of_templates=1000,overwrite_template_file=False,ignore_fixable_errors=True,only_print_image=False)
