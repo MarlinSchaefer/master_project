dfa925fd6a45de8b98fdb860054547800a95c43c
Marlin Schäfer <mb.schaefer@gmx.de>
2019-01-15 21:36:07 +0100

    Added function to load parameter space from .hf5 file

--- experimental/load_data.py
+++ experimental/load_data.py
@@ -11,3 +11,15 @@ def load_data(file_path):
     test_labels = data['testing']['test_labels'][:]
     
     return(((train_data, train_labels), (test_data, test_labels)))
+
+def load_wiki(file_path):
+    ret_dict = {}
+    
+    data = h5py.File(file_path, "r")
+    
+    for key, val in data['parameter_space'].items():
+        ret_dict[str(key)] = val.value
+    
+    data.close()
+    
+    return(ret_dict)
