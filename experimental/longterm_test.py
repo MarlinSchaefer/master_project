529e9a1a5e56277f7eb7670a13b799bc547e394c
Marlin Schäfer <mb.schaefer@gmx.de>
2019-01-22 14:26:55 +0100

    Adjusted longterm_test to show and test features.

--- experimental/longterm_test.py
+++ experimental/longterm_test.py
@@ -2,4 +2,4 @@ from run_net import run_net
 import numpy as np
 
 if __name__ == "__main__":
-    run_net('longterm_net', 'test', epochs=30, coa_phase=[0.0, np.pi],random_starting_time=True, snr=[1.0,12.0], num_of_templates=10000,overwrite_template_file=True,ignore_fixable_errors=True,only_print_image=False, use_custom_train_function=True, epoch_break=10)
+    run_net('longterm_net', 'test', epochs=50, random_starting_time=True, coa_phase=[0.0, np.pi], snr=[5.0,12.0], mass2=[10.0,30.0], mass1=[10.0,30.0], num_of_templates=10000, overwrite_template_file=True, ignore_fixable_errors=True, only_print_image=False, use_custom_train_function=True, epoch_break=5)
