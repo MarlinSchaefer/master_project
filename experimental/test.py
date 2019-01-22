8ecbb3c0eada684571dfd756d6896615d84b37ba
Marlin Schäfer <mb.schaefer@gmx.de>
2019-01-22 14:28:11 +0100

    Modified test.py to test features.

--- experimental/test.py
+++ experimental/test.py
@@ -2,4 +2,4 @@ from run_net import run_net
 import numpy as np
 
 if __name__ == "__main__":
-    run_net('varied_nothing_net_v2', 'test', epochs=10, coa_phase=[0.0, np.pi],random_starting_time=True, snr=[5.0,12.0], num_of_templates=1000,overwrite_template_file=False,ignore_fixable_errors=True,only_print_image=False)
+    run_net('varied_nothing_net_v2', 'test_2', epochs=10, coa_phase=[0.0, np.pi],random_starting_time=True, snr=[5.0,12.0], num_of_templates=20,overwrite_template_file=True,ignore_fixable_errors=False,only_print_image=False)
