eea8172999cc65dc8bd0282b16cf21c51f9bbd97
Marlin Schäfer <mb.schaefer@gmx.de>
2019-01-29 16:18:30 +0100

    Added a way to use .ini files.
    Previously all default values were set within the source code itself. This was now moved outside to the file 'defaults.ini'. There is also now an option in run_net to use a .ini file to set the values.

---
+++ vary_sky_location/ini_handeling.py
@@ -0,0 +1,43 @@
+import ConfigParser
+import os
+import ast
+
+def _section_to_dict(config, sec):
+    options = config.options(sec)
+    
+    ret = {}
+    
+    for option in options:
+        ret[option] = ast.literal_eval(config.get(sec, option))
+    
+    return(ret)
+    
+
+def load_options(file_name):
+    config = ConfigParser.ConfigParser()
+    
+    config.read(file_name)
+    
+    sec = config.sections()
+    
+    ret = {}
+    
+    for s in sec:
+        dic = _section_to_dict(config, s)
+        ret.update(dic)
+    
+    return(ret)
+
+def run_net_defaults():
+    config = ConfigParser.ConfigParser()
+    
+    config.read('defaults.ini')
+    
+    return(_section_to_dict(config, 'run_net'))
+
+def make_template_bank_defaults():
+    config = ConfigParser.ConfigParser()
+    
+    config.read('defaults.ini')
+    
+    return((_section_to_dict(config, 'make_template_bank_wav'), _section_to_dict(config, 'make_template_bank_opt')))
