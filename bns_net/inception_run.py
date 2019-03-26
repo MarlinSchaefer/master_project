e437ffa53fd1b9fa83b4a7f3c1d6616054645ff6
Marlin Schäfer <mb.schaefer@gmx.de>
2019-03-26 17:12:11 +0100

    Updated inception_run.py for test purposes.

--- bns_net/inception_run.py
+++ bns_net/inception_run.py
@@ -2,4 +2,4 @@ from run_net import run_net
 import numpy as np
 
 if __name__ == "__main__":
-    run_net('inception_net', 'mult_output_data', ini_file='inception_net.ini', snr=[10.0, 50.0], create_wiki_entry=True, overwrite_template_file=True, epochs=5, num_of_templates=100, format_data=True, epoch_break=3, gw_prob=0.5, use_custom_compilation=True, overwrite_net_file=True, t_from_right=2.5, show_snr_plot=False)
+    run_net('inception_net', 'mult_output_data', ini_file='inception_net.ini', snr=[10.0, 50.0], create_wiki_entry=True, overwrite_template_file=True, epochs=75, num_of_templates=100, format_data=True, epoch_break=3, gw_prob=0.5, use_custom_compilation=True, overwrite_net_file=True, t_from_right=2.5, show_snr_plot=False)
