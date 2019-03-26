4a53af515a42762b943cffed1a9a6af4a6df7810
Marlin Schäfer <mb.schaefer@gmx.de>
2019-03-26 19:20:50 +0100

    Added split net.

---
+++ bns_net/testing_run_split.py
@@ -0,0 +1,5 @@
+from run_net import run_net
+import numpy as np
+
+if __name__ == "__main__":
+    run_net('testing_net_split', 'mult_output_data', ini_file='testing_net.ini', snr=[10.0, 50.0], create_wiki_entry=True, overwrite_template_file=False, epochs=80, num_of_templates=10000, format_data=True, epoch_break=2, gw_prob=0.5, use_custom_compilation=True)
