efec7dcc7f59a4fe85a591ac9e55f8b37a6f3fcf
Marlin Schäfer <mb.schaefer@gmx.de>
2019-04-02 14:31:18 +0200

    Test for bugs.

--- bns_net/generator.py
+++ bns_net/generator.py
@@ -24,33 +24,44 @@ class DataGenerator(keras.utils.Sequence):
 
     def __len__(self):
         'Denotes the number of batches per epoch'
-        return int(np.ceil(float(len(self.list_IDs)) / self.batch_size))
+        return int(np.floor(float(len(self.list_IDs)) / self.batch_size))
 
     def __getitem__(self, index):
         'Generate one batch of data'
         # Generate indexes of the batch
+        print("In get item")
         indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
-
-        # Find list of IDs
-        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+        
+        print("After setting index list")
+        
+        print("After temp index list")
+        
+        print("Now trying to call __data_generation")
 
         # Generate data
-        X, y = self.__data_generation(list_IDs_temp)
-
+        X, y = self.__data_generation(indexes)
+        
+        print("After __data_generation, pre returning")
+        
         return X, y
 
     def on_epoch_end(self):
         'Updates indexes after each epoch'
-        self.indexes = np.arange(len(self.list_IDs))
+        self.indexes = np.arange(len(self.data))
         if self.shuffle == True:
             np.random.shuffle(self.indexes)
 
     def __data_generation(self, list_IDs_temp):
         'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
         # Initialization
+        
+        print("In __data_generation")
         X = np.empty((self.batch_size, self.num_samples, self.n_channels))
+        print("Allocated X")
         y = np.empty([self.batch_size] + list(self.labels[0].shape))
-
+        
+        print("Allocated y, before loop")
+        
         # Generate data
         for i, ID in enumerate(list_IDs_temp):
             # Store sample
@@ -59,10 +70,14 @@ class DataGenerator(keras.utils.Sequence):
             # Store class
             y[i] = self.labels[ID]
         
+        print("After loop")
+        
         if not format_data == None:
             X = format_data(X)
         
         if not format_labels == None:
             y = format_labels(y)
+        
+        print("Pre returning from __data_generation")
         
         return X, y
