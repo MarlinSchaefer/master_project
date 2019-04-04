43bc6ca3037e16f8f7012028c4d99a60d4d5e0a9
Marlin Schäfer <mb.schaefer@gmx.de>
2019-04-04 18:21:27 +0200

    Updated run files.

--- bns_net/test_net_split_2_run.py
+++ bns_net/test_net_split_2_run.py
@@ -1,7 +1,7 @@
 from run_net import run_net
 import numpy as np
 
-ep = 2
+ep = 100
 
 if __name__ == "__main__":
     run_net('inception_net_2', 'mult_output_data_medium_small', ini_file='testing_net.ini', snr=[10.0, 50.0], create_wiki_entry=True, overwrite_template_file=False, epochs=ep, num_of_templates=20000, format_data=True, epoch_break=5, gw_prob=0.5, use_custom_compilation=True, show_snr_plot=False, batch_size=32)
