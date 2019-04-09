3fefd9b62cf193f7950954d9d0227586c6c0adeb
Marlin Schäfer <mb.schaefer@gmx.de>
2019-04-09 15:29:59 +0200

    Trying shallower network.

--- bns_net/saves/inception_net_6_skip.py
+++ bns_net/saves/inception_net_6_skip.py
@@ -44,11 +44,7 @@ def get_model():
     
     incp_4 = incp_lay(incp_3, 32)
     
-    incp_5 = incp_lay(incp_4, 32)
-    
-    incp_6 = incp_lay(incp_5, 32)
-    
-    norm = keras.layers.BatchNormalization()(incp_6)
+    norm = keras.layers.BatchNormalization()(incp_4)
     
     pool_4 = keras.layers.MaxPooling1D(4)(norm)
     
