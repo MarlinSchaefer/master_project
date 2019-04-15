4dcd3daf9699ca121958dd5739986a792b685839
Marlin Schäfer <mb.schaefer@gmx.de>
2019-04-15 11:34:11 +0200

    Updated run files.

--- bns_net/test_net_split_2_run.py
+++ bns_net/test_net_split_2_run.py
@@ -23,12 +23,12 @@ if __name__ == "__main__":
     #except:
         #pass
     
-    run_net('collect_inception_net_6', 'mult_output_data_medium_small', ini_file='testing_net.ini', snr=[10.0, 50.0], create_wiki_entry=wiki_e, overwrite_template_file=False, epochs=150, num_of_templates=200, format_data=True, epoch_break=3, gw_prob=0.5, use_custom_compilation=True, show_snr_plot=False, batch_size=32, custom_message='This net takes all frequency channels as input. It combines the idea of collect_net with the method of the inception layers and tries to incorporate the findings of 05.04.2019 by givining higher frequencies shallower networks, whilest having deeper networks in the arms of lower frequencies.')
+    run_net('collect_inception_net_6_rev_2', 'mult_output_data_medium_small', ini_file='testing_net.ini', snr=[10.0, 50.0], create_wiki_entry=wiki_e, overwrite_template_file=False, epochs=150, num_of_templates=200, format_data=True, epoch_break=3, gw_prob=0.5, use_custom_compilation=True, show_snr_plot=False, batch_size=32, custom_message='This network in architecture is close to the inception_net_6_skip, but adds 2 convolution layers in the beginning of each step, as those showed promising results in the pure inception nets.')
     
-    try:
-        run_net('inception_net_6_skip', 'mult_output_data_medium_small', ini_file='testing_net.ini', snr=[10.0, 50.0], create_wiki_entry=wiki_e, overwrite_template_file=False, epochs=300, num_of_templates=20000, format_data=True, epoch_break=5, gw_prob=0.5, use_custom_compilation=True, show_snr_plot=False, batch_size=32, custom_message='This net takes as input the channels 2048Hz, 512Hz, 128Hz. Reduced the depth of the model to 4 inception layers to see if that improves things. The reason for this decision is the results from 05.04.2019 as they can be found in the wiki.')
-    except:
-        pass
+    #try:
+        #run_net('inception_net_6_skip', 'mult_output_data_medium_small', ini_file='testing_net.ini', snr=[10.0, 50.0], create_wiki_entry=wiki_e, overwrite_template_file=False, epochs=300, num_of_templates=20000, format_data=True, epoch_break=5, gw_prob=0.5, use_custom_compilation=True, show_snr_plot=False, batch_size=32, custom_message='This net takes as input the channels 2048Hz, 512Hz, 128Hz. Reduced the depth of the model to 4 inception layers to see if that improves things. The reason for this decision is the results from 05.04.2019 as they can be found in the wiki.')
+    #except:
+        #pass
     
     #try:
         #run_net('inception_net_2_small', 'mult_output_data_medium_small', ini_file='testing_net.ini', snr=[10.0, 50.0], create_wiki_entry=wiki_e, overwrite_template_file=False, epochs=ep, num_of_templates=20000, format_data=True, epoch_break=5, gw_prob=0.5, use_custom_compilation=True, show_snr_plot=False, batch_size=32)
