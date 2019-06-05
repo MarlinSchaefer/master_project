52cb899997ba67d95f648cf71cfd1217331d1367
Marlin Schäfer <mb.schaefer@gmx.de>
2019-06-05 12:30:57 +0200

    Updated run files.

--- bns_net/test_net_split_2_run.py
+++ bns_net/test_net_split_2_run.py
@@ -9,7 +9,8 @@ ep = 150
 wiki_e = True
 
 if __name__ == "__main__":
-    run_net('tcn_net_rev_1', 'templates_new', ini_file='testing_net.ini', create_wiki_entry=False, overwrite_template_file=False, epochs=24, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24)
+    msg = 'The purpose of this network is to try and train the first stage (the TCN part) with respect to mean average percentage error, as its output will be close to one. This in turn means, that a mean squared error of ~1 is on the order of the input and hence the network does not learn a whole lot.'
+    run_net('tcn_net_rev_2', 'templates_new', ini_file='testing_net.ini', create_wiki_entry=False, overwrite_template_file=False, epochs=24, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
     #try:
         #run_net('collect_inception_net_6_rev_6', 'small_snr_7_100000', ini_file='testing_net.ini', snr=[8.0, 15.0], create_wiki_entry=wiki_e, overwrite_template_file=True, epochs=0, num_of_templates=100000, gw_prob=0.5, custom_message='Generated large template file at more realistic SNRs.')
     #except:
