8d3341e5d3ac6435a5483feb5859a3f39e4416a6
Marlin Schäfer <mb.schaefer@gmx.de>
2019-06-18 14:00:06 +0200

    Updated run files for decaying learning rate.

--- bns_net/saves/collect_inception_net_6_rev_6.py
+++ bns_net/saves/collect_inception_net_6_rev_6.py
@@ -4,13 +4,15 @@ import json
 import os
 import load_data
 import generator as g
+from data_object import DataSet
 
-def incp_lay(x, filter_num, active_filter_sizes=(4, 8, 16), dilation_rate=(1, 1, 1)):
-    l = keras.layers.Conv1D(3 * filter_num, active_filter_sizes[0], dilation_rate=dilation_rate[0], padding='same', activation='relu')(x)
+def incp_lay(x, filter_num):
+    active_filter_sizes = (4, 8, 16)
+    l = keras.layers.Conv1D(3 * filter_num, active_filter_sizes[0], padding='same', activation='relu')(x)
     lm_1 = keras.layers.Conv1D(int(round(filter_num / 2.0)), 1, activation='relu')(x)
-    lm_2 = keras.layers.Conv1D(2 * filter_num, active_filter_sizes[1], dilation_rate=dilation_rate[1], padding='same', activation='relu')(lm_1)
+    lm_2 = keras.layers.Conv1D(2 * filter_num, active_filter_sizes[1], padding='same', activation='relu')(lm_1)
     rm_1 = keras.layers.Conv1D(int(round(filter_num / 2.0)), 1, activation='relu')(x)
-    rm_2 = keras.layers.Conv1D(filter_num, active_filter_sizes[2], dilation_rate=dilation_rate[2], padding='same', activation='relu')(rm_1)
+    rm_2 = keras.layers.Conv1D(filter_num, active_filter_sizes[2], padding='same', activation='relu')(rm_1)
     r_1 = keras.layers.MaxPooling1D(4, strides=1, padding='same')(x)
     r_2 = keras.layers.Conv1D(int(round(filter_num)), 1, activation='relu')(r_1)
     
@@ -28,34 +30,43 @@ def stack(x, NUM_DETECTORS, DROPOUT_RATE):
     conv_2 = keras.layers.Conv1D(128, 16)(pool_conv_1)
     bn_conv_2 = keras.layers.BatchNormalization()(conv_2)
     act_conv_2 = keras.layers.Activation('relu')(bn_conv_2)
-    inc_1 = incp_lay(act_conv_2, 32, active_filter_sizes=(4, 4, 4), dilation_rate=(1, 2, 3))
+    inc_1 = incp_lay(act_conv_2, 32)
     batch_2 = keras.layers.BatchNormalization()(inc_1)
-    inc_2 = incp_lay(batch_2, 32, active_filter_sizes=(4, 4, 4), dilation_rate=(1, 2, 3))
+    inc_2 = incp_lay(batch_2, 32)
     pool_1 = keras.layers.MaxPooling1D(2)(inc_2)
     batch_3 = keras.layers.BatchNormalization()(pool_1)
-    inc_3 = incp_lay(batch_3, 32, active_filter_sizes=(4, 4, 4), dilation_rate=(1, 2, 3))
+    inc_3 = incp_lay(batch_3, 32)
     batch_4 = keras.layers.BatchNormalization()(inc_3)
     return(batch_4)
+
+def get_input(input_names, NUM_OF_DETECTORS=2):
+    inp_sig = keras.layers.Input(shape=(4096, NUM_OF_DETECTORS), name=input_names[0])
+    inp_noi = keras.layers.Input(shape=(4096, NUM_OF_DETECTORS), name=input_names[1])
+    add = keras.layers.Add()([inp_sig, inp_noi])
+    return((add, inp_sig, inp_noi))
 
 def get_model():
     NUM_DETECTORS = 2
     DROPOUT_RATE = 0.25
     
-    #DATA NORMAMLIZATION AND NOISE INPUT
-    #inp_1s = keras.layers.Input(shape=(4096, NUM_DETECTORS, ), name='inp_1s')
-    inp_2s = keras.layers.Input(shape=(4096, NUM_DETECTORS, ), name='inp_2s')
-    #inp_4s = keras.layers.Input(shape=(4096, NUM_DETECTORS, ), name='inp_4s')
-    inp_8s = keras.layers.Input(shape=(4096, NUM_DETECTORS, ), name='inp_8s')
-    #inp_16s = keras.layers.Input(shape=(4096, NUM_DETECTORS, ), name='inp_16s')
-    inp_32s = keras.layers.Input(shape=(4096, NUM_DETECTORS, ), name='inp_32s')
-    #inp_64s = keras.layers.Input(shape=(4096, NUM_DETECTORS, ), name='inp_64s')
+    inp1, is1, in1 = get_input(("Signal_1s", "Noise_1s"))
+    inp2, is2, in2 = get_input(("Signal_2s", "Noise_2s"))
+    inp4, is4, in4 = get_input(("Signal_4s", "Noise_4s"))
+    inp8, is8, in8 = get_input(("Signal_8s", "Noise_8s"))
+    inp16, is16, in16 = get_input(("Signal_16s", "Noise_16s"))
+    inp32, is32, in32 = get_input(("Signal_32s", "Noise_32s"))
+    inp64, is64, in64 = get_input(("Signal_64s", "Noise_64s"))
     
-    stack_2s = stack(inp_2s, NUM_DETECTORS, DROPOUT_RATE)
-    stack_8s = stack(inp_8s, NUM_DETECTORS, DROPOUT_RATE)
-    stack_32s = stack(inp_32s, NUM_DETECTORS, DROPOUT_RATE)
+    stack_1s = stack(inp1, NUM_DETECTORS, DROPOUT_RATE)
+    stack_2s = stack(inp2, NUM_DETECTORS, DROPOUT_RATE)
+    stack_4s = stack(inp4, NUM_DETECTORS, DROPOUT_RATE)
+    stack_8s = stack(inp8, NUM_DETECTORS, DROPOUT_RATE)
+    stack_16s = stack(inp16, NUM_DETECTORS, DROPOUT_RATE)
+    stack_32s = stack(inp32, NUM_DETECTORS, DROPOUT_RATE)
+    stack_64s = stack(inp64, NUM_DETECTORS, DROPOUT_RATE)
     
     #COMBINED OUTPUT
-    combined = keras.layers.concatenate([stack_2s, stack_8s, stack_32s])
+    combined = keras.layers.concatenate([stack_1s, stack_2s, stack_4s, stack_8s, stack_16s, stack_32s, stack_64s])
     
     inc_4 = incp_lay(combined, 32)
     batch_4 = keras.layers.BatchNormalization()(inc_4)
@@ -73,23 +84,14 @@ def get_model():
     dense_3 = keras.layers.Dense(3)(flatten)
     dense_4 = keras.layers.Dense(2, activation='softmax', name='Out_Bool')(dense_3)
     
-    #model = keras.models.Model(inputs=[inp_1s, inp_2s, inp_4s, inp_8s, inp_16s, inp_32s, inp_64s], outputs=[dense_2, dense_4])
-    model = keras.models.Model(inputs=[inp_2s, inp_8s, inp_32s], outputs=[dense_2, dense_4])
+    model = keras.models.Model(inputs=[inp_1s, inp_2s, inp_4s, inp_8s, inp_16s, inp_32s, inp_64s], outputs=[dense_2, dense_4])
+    #model = keras.models.Model(inputs=[inp_2s, inp_8s, inp_32s], outputs=[dense_2, dense_4])
     
     return(model)
 
 def compile_model(model):
-    model.compile(loss={'Out_SNR': 'mean_squared_error', 'Out_Bool': 'categorical_crossentropy'}, loss_weights={'Out_SNR': 1.0, 'Out_Bool': 0.5}, optimizer='adam', metrics={'Out_SNR': 'mape', 'Out_Bool': 'accuracy'})
-
-def get_formatted_data(file_path):
-    tr_d = format_data_segment(load_data.load_training_data(file_path))
-    tr_l = format_label_segment(load_data.load_training_labels(file_path))
-    
-    te_d = format_data_segment(load_data.load_testing_data(file_path))
-    te_l = format_label_segment(load_data.load_testing_labels(file_path))
-    
-    return(((tr_d, tr_l), (te_d, te_l)))
-    
+    optimizer = keras.optimizers.Adam(decay=0.1)
+    model.compile(loss=['mean_squared_error', 'categorical_crossentropy'], loss_weights=[1.0, 0.5], optimizer=optimizer, metrics={'Out_SNR': 'mape', 'Out_Bool': 'accuracy'})
 
 def evaluate_overfitting(train_loss, test_loss):
     THRESHOLD = 0.7
@@ -113,28 +115,251 @@ def evaluate_overfitting(train_loss, test_loss):
     
     return(True)
 
-def format_data_segment(data):
-    tmp = data.transpose((2, 1, 0))
-    ret = [np.zeros((2, len(data[0]), len(data))) for i in range(3)]
-    ret[0][0] = tmp[1]
-    ret[0][1] = tmp[8]
+def get_data_obj(file_path):
+    #This is a real dirty hack to the data_object.DataSet. It does
+    #require a very special generator to work in the run_net framework.
+    class CustomDataSet():
+        def __init__(self, file_path):
+            self.file_path = file_path
+            self.load_data()
+        
+        def load_data(self):
+            with h5py.File(self.file_path) as FILE:
+                self.signals = FILE['signals']['data'][:]
+                self.noise = FILE['noise']['data'][:]
+                self.signal_labels = FILE['signals']['snr'][:]
+                self.noise_label = FILE['noise']['snr'][0]
+        
+        @property
+        def loaded_train_data(self):
+            return((self.signals, self.noise, self.signal_labels, self.training_indices, self.noise_label))
+        
+        @property
+        def loaded_test_data(self):
+            return((self.signals, self.noise, self.signal_labels, self.testing_indices, self.noise_label))
+        
+        @property
+        def loaded_test_labels(self):
+            return(self.testing_labels)
+        
+        @property
+        def loaded_train_labels(self):
+            return(self.training_labels)
+        
+        @property
+        def loaded_train_snr(self):
+            return(np.zeros(len(self.training_indices)))
+        
+        @property
+        def loaded_test_snr(self):
+            return(np.zeros(len(self.testing_indices)))
+        
+        def get_set(self, slice=None):
+            if slice == None:
+                #num_pairs = len(self.signals) * len(self.noise) / 2
+                num_noise = int(float(len(self.noise)) * 3 / 4)
+                num_signals = num_noise
+            elif type(slice) in [tuple, list] and len(slice) == 2:
+                num_signals = slice[0]
+                num_noise = slice[1]
+            else:
+                raise ValueError('slice needs to be a tuple or list of exactly 2 items.')
+            
+            signal_indices = self.generate_unique_index_pairs(2*num_signals)
+            noise_indices = np.arange(0, len(self.noise), dtype=int)
+            np.random.shuffle(noise_indices)
+            noise_indices = [(-1, pt) for pt in noise_indices]
+            self.training_indices = np.array(signal_indices[:num_signals] + noise_indices[:num_noise])
+            np.random.shuffle(self.training_indices)
+            self.training_indices = [(pt[0], pt[1]) for pt in self.training_indices]
+            self.testing_indices = np.array(signal_indices[num_signals:] + noise_indices[num_noise:])
+            np.random.shuffle(self.testing_indices)
+            self.testing_indices = [(pt[0], pt[1]) for pt in self.testing_indices]
+            
+            training_snrs = np.array([[self.signal_labels[i[0]]] if not i[0] == -1 else [self.noise_label] for i in self.training_indices])
+            training_bool = np.array([[1.0, 0.0] if not i[0] == -1 else [0.0, 1.0] for i in self.training_indices])
+            
+            testing_snrs = np.array([[self.signal_labels[i[0]]] if not i[0] == -1 else [self.noise_label] for i in self.testing_indices])
+            testing_bool = np.array([[1.0, 0.0] if not i[0] == -1 else [0.0, 1.0] for i in self.testing_indices])
+            
+            self.training_labels = [training_snrs, training_bool]
+            self.testing_labels = [testing_snrs, testing_bool]
+        
+        def get_file_properties(self):
+            return({'snr': [8.0, 15.0]})
+        
+        def generate_unique_index_pairs_noise(self, num_pairs):
+            if len(self.noise) < num_pairs:
+                raise ValueError("Can't generate more indices for pure noise than there are noise instances.")
+            
+            if num_pairs < len(self.noise) / 2:
+                invert = False
+            else:
+                invert = True
+                num_pairs = len(self.noise) - num_pairs
+            
+            curr_pairs = 0
+            poss = np.zeros(len(self.noise))
+            while curr_pairs < num_pairs:
+                r_int = np.random.randint(0, len(self.noise))
+                if poss[r_int] == 0:
+                    poss[r_int] = 1
+                    curr_pairs += 1
+            
+            true_val = 0 if invert else 1
+            
+            ret = []
+            
+            for i, val in enumerate(poss):
+                if val == true_val:
+                    ret.append((-1, i))
+            
+            ret = np.array(ret, dtype=int)
+            np.random.shuffle(ret)
+            
+            ret = [(pt[0], pt[1]) for pt in ret]
+            
+            return(ret)
+        
+        def generate_unique_index_pairs(self, num_pairs, generate_signals_only=True):
+            len_sig = len(self.signals)
+            len_noi = len(self.noise)
+            if generate_signals_only:
+                max_len = len_sig * len_noi
+            else:
+                max_len = (len_sig+1) * len_noi
+                
+            if max_len < num_pairs:
+                raise ValueError('Tried to generate {} unique pairs from {} possible combinations.'.format(num_pairs, max_len))
+            
+            #Check if it is more efficient to pick pairs to not include
+            inv = False
+            if num_pairs > (len_sig * len_noi) / 2:
+                inv = True
+            
+            curr_pairs = 0
+            max_pair = max_len - num_pairs if inv else num_pairs
+            poss = np.zeros(max_len, dtype=int)
+            while curr_pairs < max_pair:
+                r_int = np.random.randint(0, max_len)
+                if poss[r_int] == 0:
+                    poss[r_int] = 1
+                    curr_pairs += 1
+            
+            true_val = 0 if inv else 1
+            
+            ret = []
+            
+            for i, val in enumerate(poss):
+                if val == true_val:
+                    sig_idx = i / len_noi
+                    noi_idx = i % len_noi
+        
+                    if sig_idx == len_sig:
+                        ret.append((-1, noi_idx))
+                    else:
+                        ret.append((sig_idx, noi_idx))
+            
+            ret = np.array(ret, dtype=int)
+            np.random.shuffle(ret)
+            
+            ret = [(pt[0], pt[1]) for pt in ret]
+            
+            return(ret)
+        
+        #The definitions below might not be sensible
+        @property
+        def training_data_shape(self):
+            with h5py.File(self.file_path, 'r') as FILE:
+                return(FILE['signals']['data'].shape)
+        
+        @property
+        def training_label_shape(self):
+            with h5py.File(self.file_path, 'r') as FILE:
+                return(FILE['signals']['snr'].shape)
+        
+        @property
+        def testing_data_shape(self):
+            with h5py.File(self.file_path, 'r') as FILE:
+                return(FILE['noise']['data'].shape)
+            
+        @property
+        def training_label_shape(self):
+            with h5py.File(self.file_path, 'r') as FILE:
+                return(FILE['noise']['snr'].shape)
     
-    ret[1][0] = tmp[3]
-    ret[1][1] = tmp[10]
-    
-    ret[2][0] = tmp[5]
-    ret[2][1] = tmp[12]
-    
-    ret = [dat.transpose((2, 1, 0)) for dat in ret]
-    return(ret)
+    return(CustomDataSet(file_path))
 
-def format_label_segment(data):
-    ret = [[], []]
-    for l in data:
-        ret[0].append([l[0]])
-        ret[1].append([1, 0] if bool(l[1]) else [0, 1])
-    ret = [np.array(dat) for dat in ret]
-    return(ret)
+class DataGenerator(keras.utils.Sequence):
+    def __init__(self, dobj_data, dobj_labels, batch_size=32, shuffle=True):
+        (signals, noise, signal_labels, index_list, noise_snr) = dobj_data
+        self.signals = signals
+        self.noise = noise
+        self.signal_labels = signal_labels
+        self.index_list = index_list
+        self.noise_snr = noise_snr
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.data_channels = self.signals[0].shape[-1] / 2
+        self.on_epoch_end()
+    
+    def __len__(self):
+        return(int(np.ceil(float(len(self.index_list)) / self.batch_size)))
+    
+    def on_epoch_end(self):
+        self.indices = np.arange(len(self.index_list))
+        if self.shuffle:
+            np.random.shuffle(self.indices)
+            self.indices = list(self.indices)
+        else:
+            self.indices = list(self.indices)
+    
+    def __getitem__(self, index):
+        if (index + 1) * self.batch_size >= len(self.indices):
+            indices = self.indices[index*self.batch_size:]
+        else:
+            indices = self.indices[index*self.batch_size:(index+1)*self.batch_size]
+        
+        X, y = self.__data_generation(indices)
+        
+        return(X, y)
+    
+    def __data_generation(self, indices):
+        X = [np.zeros([len(indices), 2, self.signals[0].shape[0]]) for i in range(self.data_channels * 2)]
+        
+        y_1 = np.zeros((len(indices), 1))
+        
+        y_2 = np.zeros((len(indices), 2))
+        
+        for i, idx in enumerate(indices):
+            sig_ind, noi_ind = self.index_list[idx]
+            
+            for j in range(self.data_channels):
+                if not sig_ind == -1:
+                    X[2 * j][i][0] = self.signals[sig_ind].transpose()[j]
+                    X[2 * j][i][1] = self.signals[sig_ind].transpose()[j+self.data_channels]
+                
+                X[2 * j + 1][i][0] = self.noise[noi_ind].transpose()[j]
+                X[2 * j + 1][i][1] = self.noise[noi_ind].transpose()[j+self.data_channels]
+            
+            if not sig_ind == -1:
+                y_1[i] = self.signal_labels[sig_ind]
+                
+                y_2[i][0] = 1.0
+                y_2[i][1] = 0.0
+            else:
+                y_1[i] = self.noise_snr
+                
+                y_2[i][0] = 0.0
+                y_2[i][1] = 1.0
+            
+        
+        X = [dat.transpose(0, 2, 1) for dat in X]
+        
+        return((X, [y_1, y_2]))
+
+def get_generator():
+    return(DataGenerator)
 
 def get_model_memory_usage(batch_size, model):
     from keras import backend as K
@@ -161,9 +386,10 @@ def get_model_memory_usage(batch_size, model):
     gbytes = np.round(total_memory / (1024.0 ** 3), 3)
     return gbytes
 
-def train_model(model, data_path, net_path, epochs=None, epoch_break=10, batch_size=32):
+def train_model(model, dobj, net_path, epochs=None, epoch_break=10, batch_size=32):
     print("Epochs: {}\nEpoch_break={}".format(epochs, epoch_break))
-    name = __file__[:-4]
+    print("Net path: {}".format(net_path))
+    name = os.path.basename(__file__)[:-4]
     
     #Store the results of training (i.e. the loss)
     results = []
@@ -206,10 +432,11 @@ def train_model(model, data_path, net_path, epochs=None, epoch_break=10, batch_s
         
         print("Expected memory_size: {}".format(get_model_memory_usage(batch_size, model)))
         
-        (train_data, train_labels), (test_data, test_labels) = get_formatted_data(data_path)
+        training_generator = get_generator()
+        testing_generator = get_generator()
         
-        training_generator = g.DataGeneratorMultInput(train_data, train_labels, batch_size=batch_size)
-        testing_generator = g.DataGeneratorMultInput(test_data, test_labels, batch_size=batch_size)
+        training_generator = training_generator(dobj.loaded_train_data, dobj.loaded_train_labels, batch_size=batch_size)
+        testing_generator = testing_generator(dobj.loaded_test_data, dobj.loaded_test_labels, batch_size=batch_size)
         
         for i in range(ran):
             print("ran: {}\ni: {}".format(ran, i))
@@ -230,8 +457,12 @@ def train_model(model, data_path, net_path, epochs=None, epoch_break=10, batch_s
             print(curr_counter)
             
             #Store model after each training-cycle
-            model.save(os.path.join(net_path, name + "_epoch_" + str(curr_counter) + ".hf5"))
-            print("Stored net at: {}".format(os.path.join(net_path, name + "_epoch_" + str(curr_counter) + ".hf5")))
+            print("Net path before saving: {}".format(net_path))
+            tmp_name = str(name + "_epoch_" + str(curr_counter) + ".hf5")
+            tmp_path = os.path.join(net_path, tmp_name)
+            print("Trying to save at: {}".format(tmp_path))
+            model.save(os.path.join(net_path, tmp_name))
+            print("Stored net at: {}".format(os.path.join(net_path, tmp_name)))
             
             #Evaluate the performance of the net after every cycle and store it.
             results.append([curr_counter, model.evaluate_generator(generator=training_generator, max_q_size=q_size), model.evaluate_generator(generator=testing_generator, max_q_size=q_size)])
