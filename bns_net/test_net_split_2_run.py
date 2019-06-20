0e1c25a0befc656e5acf0abc07fcffff9662bd4d
Marlin Schäfer <mb.schaefer@gmx.de>
2019-06-20 18:19:55 +0200

    Reverted run files.

--- bns_net/test_net_split_2_run.py
+++ bns_net/test_net_split_2_run.py
@@ -10,6 +10,6 @@ ep = 150
 wiki_e = True
 
 if __name__ == "__main__":
-    msg = 'This network is basically the same as collect_inception_net_6_rev_6, but has no dropout layers anymore and leaves the Adam optimizer at stock learning rate.'
-    run_net('collect_inception_net_6_rev_8', 'templates_new', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=100, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
+    msg = 'This network is basically the same as collect_inception_net_6_rev_6, but introduces a residual connection after every inception layer. This means, that the input of every layer gets added back to the output. This is supposed to help during training.'
+    run_net('collect_inception_net_6_rev_7', 'templates_new', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=100, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
     
