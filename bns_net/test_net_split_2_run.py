e8e4f9e416d0a37713221442903d163c6de25fe8
Marlin Schäfer <mb.schaefer@gmx.de>
2019-06-21 19:28:57 +0200

    Fixed typo in run file.

--- bns_net/test_net_split_2_run.py
+++ bns_net/test_net_split_2_run.py
@@ -10,7 +10,7 @@ ep = 150
 wiki_e = True
 
 if __name__ == "__main__":
-    try
+    try:
         msg = 'This network is basically the same as collect_inception_net_6_rev_6, but has no dropout layers anymore and leaves the Adam optimizer at stock learning rate.'
         run_net('collect_inception_net_6_rev_8', 'templates_new', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=100, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
     except:
