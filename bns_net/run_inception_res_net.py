2f5abce5920582d7d4b8e63402a2865d835fe349
Marlin Schäfer <mb.schaefer@gmx.de>
2019-06-26 15:28:00 +0200

    New run files, inception-res-net.

---
+++ bns_net/run_inception_res_net.py
@@ -0,0 +1,15 @@
+from tensorflow import ConfigProto, Session
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = Session(config=config)
+from run_net import run_net
+import numpy as np
+import traceback
+
+if __name__ == "__main__":
+    try:
+        msg = 'This network tries a very deep inception-res-net. Uses only 3 channels.'
+        run_net('inception_res_net', 'templates_new', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=50, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
+    except:
+        traceback.print_exc()
+        pass
