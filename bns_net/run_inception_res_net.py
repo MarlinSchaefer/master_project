ceaffb9a312f21820b9dba63bf6c913a8ba04052
Marlin Schäfer <mb.schaefer@gmx.de>
2019-06-27 13:22:57 +0200

    New experiments on inception res net and fixing wrong connections.

--- bns_net/run_inception_res_net.py
+++ bns_net/run_inception_res_net.py
@@ -8,15 +8,15 @@ import traceback
 
 if __name__ == "__main__":
     try:
-        msg = 'The same run as on inception_res_net_rev_2_2662019224937, but with the dropout layer removed to see if that helps it learn.'
-        run_net('inception_res_net_rev_2', 'templates_new', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=50, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
+        msg = 'This network is the one inception_res_net should have been. It fixes some wrong connections in the network.'
+        run_net('inception_res_net_rev_5', 'templates_new_dev24', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=50, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
     except:
         traceback.print_exc()
         pass
     
     try:
-        msg = 'The same run as on inception_res_net_rev_4_276201913631, but with the dropout layer removed to see if that helps it learn.'
-        run_net('inception_res_net_rev_4', 'templates_new', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=50, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
+        msg = 'This network architecturally is the same as inception_res_net_rev_5, but increases the number of filters in each inception layer.'
+        run_net('inception_res_net_rev_6', 'templates_new_dev24', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=50, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
     except:
         traceback.print_exc()
         pass
