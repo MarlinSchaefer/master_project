580bb29c8a5bcdcfd002630ecef065afa4d45348
Marlin Schaefer <marlin.schaefer@atlas5>
2019-06-29 20:33:30 +0200

    Updated wiki and fixed typo.

--- bns_net/run_inception_res_net.py
+++ bns_net/run_inception_res_net.py
@@ -9,7 +9,7 @@ import traceback
 if __name__ == "__main__":
     try:
         msg = 'This is the same network as the one from 29.06.2019 (2) from the Network wiki. The difference to before is that now also the coalescence phase gets varied. (Trained using mse, metric mape)'
-        run_net('inception_res_net_rev_9', 'templates_new_vary_mass_sky_pos_coa_phase', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=50, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
+        run_net('inception_res_net_rev_9', 'templates_new_vary_mass_sky_pos_cos_phase', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=50, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
     except:
         traceback.print_exc()
         pass
