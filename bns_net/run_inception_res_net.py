b47a356f2431cb9d4a8617d0f763992e84a59d60
Marlin Schäfer <mb.schaefer@gmx.de>
2019-06-29 15:33:34 +0200

    Slightly adapted architecture to maybe improve performance.

--- bns_net/run_inception_res_net.py
+++ bns_net/run_inception_res_net.py
@@ -8,8 +8,8 @@ import traceback
 
 if __name__ == "__main__":
     try:
-        msg = 'This is the same network as the one from 27.06.2019 (3) from the Network wiki. The difference to before is the training set. It uses now also templates with varied masses and SKY POSITION. (Trained using mse, metric mape)'
-        run_net('inception_res_net_rev_7', 'templates_new_vary_mass_sky_pos', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=50, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
+        msg = 'This is the same network as the one from 29.06.2019 from the Network wiki. The difference to before is the filter sizes of the inception layers. They are adapted to (1, 2, 3), as that showed better performance before. (Trained using mse, metric mape)'
+        run_net('inception_res_net_rev_9', 'templates_new_vary_mass_sky_pos', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=50, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
     except:
         traceback.print_exc()
         pass
