8f72d6f192dc40eba84fe9a8ccab847a7c921711
Marlin Schäfer <mb.schaefer@gmx.de>
2019-07-02 22:42:11 +0200

    Updated run files.

--- bns_net/run_inception_res_net.py
+++ bns_net/run_inception_res_net.py
@@ -8,8 +8,29 @@ import traceback
 
 if __name__ == "__main__":
     try:
-        msg = 'This network tries to combine the sensitivity of inception_res_net_rev_9 with the recovery of SNR-values of collect_inception_net_rev_(x). The architecture is therefore a combination of the two architectures, utilizing what has been identified as best practice. (i.e. having residual connections, using kernel sizes (1, 2, 3), having convolution layers before the inception stack, using the same layers after concatination...)(Trained using mse, metric mape)'
-        run_net('inception_res_net_rev_10', 'templates_new_vary_mass_sky_pos_cos_phase', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=50, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
+        msg = 'This run tries to identify which data is best to be fed to the network, as there is the new and reduced data feeding. In this case we try to have just non overlapping data for all sample rates. (Trained using mse, metric mape)'
+        run_net('inception_res_net_rev_9', 'templates_new_vary_mass_sky_pos_coa_phase', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=50, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
+    except:
+        traceback.print_exc()
+        pass
+    
+    try:
+        msg = 'This run tries to identify which data is best to be fed to the network, as there is the new and reduced data feeding. In this case we try to have non overlapping data for only the sample rates 2048, 512 and 128. (Trained using mse, metric mape)'
+        run_net('inception_res_net_rev_11', 'templates_new_vary_mass_sky_pos_coa_phase', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=50, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
+    except:
+        traceback.print_exc()
+        pass
+    
+    try:
+        msg = 'This run tries to identify which data is best to be fed to the network, as there is the new and reduced data feeding. In this case we try to have overlapping data for all sample rates. (Trained using mse, metric mape)'
+        run_net('inception_res_net_rev_12', 'templates_new_vary_mass_sky_pos_coa_phase', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=50, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
+    except:
+        traceback.print_exc()
+        pass
+    
+    try:
+        msg = 'This run tries to identify which data is best to be fed to the network, as there is the new and reduced data feeding. In this case we try to have overlapping data for only the sample rates 2048, 512, 128. (Trained using mse, metric mape)'
+        run_net('inception_res_net_rev_13', 'templates_new_vary_mass_sky_pos_coa_phase', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=50, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
     except:
         traceback.print_exc()
         pass
