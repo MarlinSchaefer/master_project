55749789dc319dc83e28eab7822f94adf953a8b8
Marlin Schäfer <mb.schaefer@gmx.de>
2019-07-11 08:03:34 +0200

    Updated run files.
    Trying to train for a binary decision only.

--- bns_net/run_inception_res_net.py
+++ bns_net/run_inception_res_net.py
@@ -7,29 +7,8 @@ import numpy as np
 import traceback
 
 if __name__ == "__main__":
-    #try:
-        #msg = 'This run tries to identify which data is best to be fed to the network, as there is the new and reduced data feeding. In this case we try to have just non overlapping data for all sample rates. (Trained using mse, metric mape)'
-        #run_net('inception_res_net_rev_9', 'templates_new_vary_mass_sky_pos_coa_phase', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=50, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
-    #except:
-        #traceback.print_exc()
-        #pass
-    
-    #try:
-        #msg = 'This run tries to identify which data is best to be fed to the network, as there is the new and reduced data feeding. In this case we try to have non overlapping data for only the sample rates 2048, 512 and 128. (Trained using mse, metric mape)'
-        #run_net('inception_res_net_rev_11', 'templates_new_vary_mass_sky_pos_coa_phase', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=50, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
-    #except:
-        #traceback.print_exc()
-        #pass
-    
     try:
-        msg = 'ATTENTION: CHANGED THE DEFAULT IMPORT SIZE IN D_OBJ! Removed dropout from inception_res_net_rev_9 and increased the number of signals and noise samples to 250000 and 750000 respectively. (Trained using mse, metric mape)'
-        run_net('inception_res_net_rev_15', 'templates_new_vary_mass_sky_pos_coa_phase', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=20, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
-    except:
-        traceback.print_exc()
-        pass
-    
-    try:
-        msg = 'ATTENTION: CHANGED THE DEFAULT IMPORT SIZE IN D_OBJ! Removed dropout from collect_inception_res_net_rev_1 and increased the number of signals and noise samples to 250000 and 750000 respectively. (Trained using mse, metric mape)'
+        msg = 'ATTENTION: CHANGED THE DEFAULT IMPORT SIZE IN D_OBJ! Trying to train collect_inception_res_net_rev_2 only for a binary decision. (Trained using mse, metric mape)'
         run_net('collect_inception_res_net_rev_2', 'templates_new_vary_mass_sky_pos_coa_phase', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=20, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
     except:
         traceback.print_exc()
