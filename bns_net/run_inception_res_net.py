d97d9fa51d5d9bb1015d3d85a7c5d01686a8c01a
Marlin Schäfer <mb.schaefer@gmx.de>
2019-07-11 08:07:00 +0200

    Fixed template file name.

--- bns_net/run_inception_res_net.py
+++ bns_net/run_inception_res_net.py
@@ -9,7 +9,7 @@ import traceback
 if __name__ == "__main__":
     try:
         msg = 'ATTENTION: CHANGED THE DEFAULT IMPORT SIZE IN D_OBJ! Trying to train collect_inception_res_net_rev_2 only for a binary decision. (Trained using mse, metric mape)'
-        run_net('collect_inception_res_net_rev_2', 'templates_new_vary_mass_sky_pos_coa_phase', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=20, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
+        run_net('collect_inception_res_net_rev_2', 'templates_new_vary_mass_sky_pos_coa_phase_large', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=20, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
     except:
         traceback.print_exc()
         pass
