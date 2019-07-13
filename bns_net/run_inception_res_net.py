aa3a7457103434055dd67af7b267e4b81e10495a
Marlin Schäfer <mb.schaefer@gmx.de>
2019-07-13 13:54:02 +0200

    New run files.

--- bns_net/run_inception_res_net.py
+++ bns_net/run_inception_res_net.py
@@ -8,8 +8,8 @@ import traceback
 
 if __name__ == "__main__":
     try:
-        msg = 'ATTENTION: CHANGED THE DEFAULT IMPORT SIZE IN D_OBJ! Trying to use the FConv layer as a means to improve the performace of the network. Also increased filter sizes in the inception layers to (2, 3, 4) and the loss weight for the binary decision to 2.0 from 0.5. (Trained using mse, metric mape)'
-        run_net('collect_inception_res_net_fconv_rev_1', 'templates_new_vary_mass_sky_pos_coa_phase_large', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=40, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
+        msg = 'ATTENTION: CHANGED THE DEFAULT IMPORT SIZE IN D_OBJ! Trying to use the FConv layer as a means to improve the performace of the network. Also increased filter sizes in the inception layers to (2, 3, 4) and the loss weight for the binary decision to 2.0 from 0.5. Added Dropout back in, as it showed improvements before. Furthermore decreased the size of the first convolution filter by a factor of 2 and put a BatchNormalization before it. [Comparing to collect_inception_res_net_fconv_rev_1](Trained using mse, metric mape)'
+        run_net('collect_inception_res_net_fconv_rev_2', 'templates_new_vary_mass_sky_pos_coa_phase_large', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=40, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
     except:
         traceback.print_exc()
         pass
