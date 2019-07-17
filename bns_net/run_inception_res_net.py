10dcbfa0c48a15ada8ab0d8053ea5791db6b9b11
Marlin Schäfer <mb.schaefer@gmx.de>
2019-07-17 14:02:19 +0200

    Changed learning rate.

--- bns_net/run_inception_res_net.py
+++ bns_net/run_inception_res_net.py
@@ -8,8 +8,9 @@ import traceback
 
 if __name__ == "__main__":
     try:
-        msg = 'ATTENTION: CHANGED THE DEFAULT IMPORT SIZE IN D_OBJ! This network is collect_inception_res_net_rev_5 from the prior run, but removed the activation function on the final layer of the SNR output. (Trained using mse, metric mape)'
+        msg = 'ATTENTION: CHANGED THE DEFAULT IMPORT SIZE IN D_OBJ! This network is collect_inception_res_net_rev_5 from the prior run, but uses the default Adam optimizer instead of one with the learning rate lowered to 1e-6. (Trained using mse, metric mape)'
         run_net('collect_inception_res_net_rev_5', 'templates_new_vary_mass_sky_pos_coa_phase_large', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=40, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
+        #run_net('collect_inception_res_net_rev_5', 'templates_new', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=40, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
     except:
         traceback.print_exc()
         pass
