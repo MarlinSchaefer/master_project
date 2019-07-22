b002a07ecf613726923ea71bec28e071627df99f
Marlin Schäfer <mb.schaefer@gmx.de>
2019-07-22 14:12:35 +0200

    Updated run files: Trying simple case.

--- bns_net/run_inception_res_net.py
+++ bns_net/run_inception_res_net.py
@@ -8,8 +8,8 @@ import traceback
 
 if __name__ == "__main__":
     try:
-        msg = 'ATTENTION: CHANGED THE DEFAULT IMPORT SIZE IN D_OBJ! This network is collect_inception_res_net_rev_6 from the prior run, but uses mean squared error as the loss function. (To compare against convolution layer.)'
-        run_net('collect_inception_res_net_rev_6', 'templates_new_vary_mass_sky_pos_coa_phase_large', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=40, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
+        msg = 'ATTENTION: CHANGED THE DEFAULT IMPORT SIZE IN D_OBJ! This network is the same as 22.07.2019 but trained using mean squared error as loss and basic templates, that only vary the SNR (i.e. distance).'
+        run_net('collect_inception_res_net_rev_5', 'templates_new_dev25', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=40, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
     except:
         traceback.print_exc()
         pass
