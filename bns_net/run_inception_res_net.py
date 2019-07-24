6a3ad16dd5682ef3c563cd13f73b241cd07e4b06
Marlin Schäfer <mb.schaefer@gmx.de>
2019-07-24 09:37:07 +0200

    Updated run files: Old inception stack

--- bns_net/run_inception_res_net.py
+++ bns_net/run_inception_res_net.py
@@ -8,8 +8,8 @@ import traceback
 
 if __name__ == "__main__":
     try:
-        msg = 'ATTENTION: CHANGED THE DEFAULT IMPORT SIZE IN D_OBJ! This network is the same as 22.07.2019 but trained using a custom loss (loss_c1) as loss and basic templates, that only vary the SNR (i.e. distance). It also reintroduces the ReLU-activation function on the last layer. It is to be compared to collect_inception_res_net_rev_5_2272019121321, as that uses mse instead of the custom loss.'
-        run_net('collect_inception_res_net_rev_5', 'templates_new_dev24', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=40, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
+        msg = 'ATTENTION: CHANGED THE DEFAULT IMPORT SIZE IN D_OBJ! This network is a pure inception layer stack. On more difficult data it was worse than the collect_inception_res_nets, but we want to try it also on the easier case. Trained using mse, has dropout.'
+        run_net('inception_res_net_rev_9', 'templates_new_dev24', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=40, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
     except:
         traceback.print_exc()
         pass
