c409b413cc33753c6a33cd4d9ea8f7fbca43b4bc
Marlin Schäfer <mb.schaefer@gmx.de>
2019-07-28 12:51:22 +0200

    Updated run files: Smaller architecture

--- bns_net/run_inception_res_net.py
+++ bns_net/run_inception_res_net.py
@@ -9,7 +9,7 @@ import traceback
 if __name__ == "__main__":
     try:
         msg = 'ATTENTION: CHANGED THE DEFAULT IMPORT SIZE IN D_OBJ! This run tries to use mostly just the TCN-network and funnels it to the SNR-detection quite quickly.'
-        run_net('tcn_funnel_rev_1', 'templates_new_dev24', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=40, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
+        run_net('tcn_collect_inception_res_net_rev_3', 'templates_new', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=40, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
     except:
         traceback.print_exc()
         pass
