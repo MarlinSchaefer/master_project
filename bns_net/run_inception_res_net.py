4ab968b55e49bbe38616c543c5ef2c6067678b6f
Marlin Schäfer <mb.schaefer@gmx.de>
2019-09-04 15:01:38 +0200

    Updated run files: TCN-collect-inception-res-net-rev-7

--- bns_net/run_inception_res_net.py
+++ bns_net/run_inception_res_net.py
@@ -8,8 +8,8 @@ import traceback
 
 if __name__ == "__main__":
     try:
-        msg = 'ATTENTION: CHANGED THE DEFAULT IMPORT SIZE IN D_OBJ! This run varies only mass and sky position but not the coalesence phase.'
-        run_net('tcn_collect_inception_res_net_rev_2', 'templates_new_vary_mass_sky_pos', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=20, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=24, custom_message=msg)
+        msg = 'ATTENTION: CHANGED THE DEFAULT IMPORT SIZE IN D_OBJ! This run is supposed to cross-check the first "final" run, i.e. tcn_collect_inception_res_net_rev_6. It uses the same data, but less and also checkpoints every epoch. It removes auxilliary outputs and introduces two further inception layers after the final concatenation.'
+        run_net('tcn_collect_inception_res_net_rev_7', 'templates_more_right_vary_all_final', ini_file='testing_net.ini', create_wiki_entry=True, overwrite_template_file=False, epochs=20, use_data_object=True, show_snr_plot=False, overwrite_net_file=True, evaluate_on_large_testing_set=False, batch_size=16, custom_message=msg)
     except:
         traceback.print_exc()
         pass
