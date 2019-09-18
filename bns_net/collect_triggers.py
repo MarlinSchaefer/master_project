9a855dfe020ce7fcf0ac0a90cc7cb9c0b5b494b7
Marlin Schäfer <mb.schaefer@gmx.de>
2019-09-18 14:34:22 +0200

    Major updates to file.

--- bns_net/collect_triggers.py
+++ bns_net/collect_triggers.py
@@ -2,6 +2,8 @@ import h5py
 import numpy as np
 from aux_functions import get_store_path
 import os
+from progress_bar import progress_tracker
+from pycbc.sensitivity import volume_montecarlo
 
 def get_results_path():
     return os.path.join(get_store_path(), 'long_data_2', 'results')
@@ -20,6 +22,9 @@ def start_time(highLevel, lowLevel):
     #Trigger time addative, positions the triggers correctly
     trig_time_add = 65.5
     return((idxHigh * 22 + lowLevel) * t_part + trig_time_add)
+
+def mchirp(m1, m2):
+    return (m1 * m2) ** (3. / 5.) / (m1 + m2) ** (1. / 5.)
 
 def collectCombinedTriggers():
     clearList = list(np.arange(3, 202, 2))
@@ -189,7 +194,7 @@ def clusterTriggerTimes(triggerTimes, time_span=0.5):
                 
     return indexRanges
 
-def getTriggersAtThreshold(threshold, mode='snr', symmetricInjectionWindow=3., highCutoff=np.inf):
+def getTriggersAtThreshold(threshold, mode='snr', symmetricInjectionWindow=3., highCutoff=np.inf, clusterTime=1.0):
     """Get a list of SNR triggers above a threshold value.
     
     Arguments
@@ -203,13 +208,22 @@ def getTriggersAtThreshold(threshold, mode='snr', symmetricInjectionWindow=3., h
         from the data is still considered to belong to the injection
     highCutoff : {optional, float}
         Value above which no trigger is generated
+    clusterTime : {optional, float}
+        Time two individual triggers are allowed to be separated by and
+        still belong to the same cluster. If this value is smaller than
+        0.25 all triggers are treated individually and no clustering
+        takes place.
     
     Returns
     -------
     truePos : List
-        List of triggers that where correctly identified
+        List of triggers that where correctly identified. Each entry is
+        a tuple containing (injection time, recovered time, recovered
+        value at that time)
     falsePos : List
-        List of tirggers that do not belong to an injection
+        List of tirggers that do not belong to an injection. Each entry
+        is a tuple containing (recovered time, distance to nearest
+        injection, value at recovered time)
     missed:
         List of injection times that were not found
     """
@@ -248,7 +262,7 @@ def getTriggersAtThreshold(threshold, mode='snr', symmetricInjectionWindow=3., h
     
     #Cluster the triggers, use the symmetricInjectionWindow as a time
     #during which the triggers belong to the same event
-    clusters = clusterTriggerTimes(triggerTimes, time_span=1.0)
+    clusters = clusterTriggerTimes(triggerTimes, time_span=clusterTime)
     
     #Find the actual trigger times and veto thos with too high values
     finalTimes = []
@@ -266,18 +280,13 @@ def getTriggersAtThreshold(threshold, mode='snr', symmetricInjectionWindow=3., h
             pass
     
     #Find if trigger corresponds to actual injection
-    for trig in finalTimes:
-        found = False
-        min_distance = np.inf
-        for inj in injectionTimes:
-            if abs(trig - inj) <= symmetricInjectionWindow:
-                truePos.append((inj, trig))
-                found = True
-            else:
-                if abs(trig - inj) < abs(min_distance):
-                    min_distance = inj - trig
-        if not found:
-            falsePos.append((trig, min_distance))
+    for trigIdx, trig in enumerate(finalTimes):
+        tmp = np.abs(trig - injectionTimes)
+        min_idx = np.argmin(tmp)
+        if tmp[min_idx] <= symmetricInjectionWindow:
+            truePos.append((tmp[min_idx], trig, finalVals[trigIdx]))
+        else:
+            falsePos.append((trig, injectionTimes[min_idx] - trig, finalVals[trigIdx]))
     
     #Find missed injections
     foundInj = [pt[0] for pt in truePos]
@@ -287,7 +296,46 @@ def getTriggersAtThreshold(threshold, mode='snr', symmetricInjectionWindow=3., h
     
     return (truePos, falsePos, missed)
 
-def getCombinedTriggersAtThreshold(threshold_snr, threshold_p, symmetricInjectionWindow=3., highCutoffSnr=np.inf, highCutoffP=np.inf):
+def getCombinedTriggersAtThreshold(threshold_snr, threshold_p, mode='snr', symmetricInjectionWindow=3., highCutoff=np.inf, clusterTime=1.0):
+    """Get only triggers, that are triggers in both outputs.
+    
+    Arguments
+    ---------
+    threshold_snr : float
+        The threshold above which a value is considered a trigger in the
+        SNR time series
+    threshold_p : float
+        The threshold above which a value is considered a trigger in the
+        p-value time series
+    mode : {optional, str}
+        One of 'snr', 'p-score'. Which of the two trigger values is used
+        to find the maximum and thus the time position.
+    symmetricInjectionWindow : {optional, float}
+        Time window around injection, in which we consider a trigger to
+        belong to that injection
+    highCutoff : {optional, float}
+        Above which value triggers in the specific mode are ignored when
+        finding the maximum value in a trigger interval
+    clusterTime : {optional, float}
+        Time two individual triggers are allowed to be separated by and
+        still belong to the same cluster. If this value is smaller than
+        0.25 all triggers are treated individually and no clustering
+        takes place.
+    
+    Returns
+    -------
+    truePos : List
+        List of triggers that where correctly identified. Each entry is
+        a tuple containing (injection time, recovered time, SNR value at
+        recovered time, p-score at recovered time)
+    falsePos : List
+        List of tirggers that do not belong to an injection. Each entry
+        is a tuple containing (recovered time, distance to nearest
+        injection, SNR value at recovered time, p-score at recovered
+        time)
+    missed:
+        List of injection times that were not found
+    """
     truePos = []
     falsePos = []
     missed = []
@@ -296,54 +344,91 @@ def getCombinedTriggersAtThreshold(threshold_snr, threshold_p, symmetricInjectio
     
     allowed = availableData()
     
-    triggerTimes = []
-    triggerVals = []
+    snrTriggerTimes = []
+    snrTriggerVals = []
     with h5py.File(get_collect_path(), 'r') as f:
         for highLevel in sorted(allowed.keys()):
             for lowLevel in sorted(allowed[highLevel]):
-                snr = f[str(highLevel)][str(lowLevel)][ts_data][:]
-                times = f[str(highLevel)][str(lowLevel)][ts_times][:]
+                snr = f[str(highLevel)][str(lowLevel)]['TimeSeries/snrTimeSeries/data'][:]
+                times = f[str(highLevel)][str(lowLevel)]['TimeSeries/snrTimeSeries/sample_times'][:]
                 times = boundaries(highLevel, lowLevel)[0] + 65.5 + times
                 #Do I want the high cutoff here or do I veto it later?
-                triggerIdx = np.where(snr > threshold)[0]
-                triggerTimes = triggerTimes + list(times[triggerIdx])
-                triggerVals = triggerVals + list(snr[triggerIdx])
+                triggerIdx = np.where(snr > threshold_snr)[0]
+                snrTriggerTimes = snrTriggerTimes + list(times[triggerIdx])
+                snrTriggerVals = snrTriggerVals + list(snr[triggerIdx])
+    
+    snrTriggerTimes = np.array(snrTriggerTimes)
+    snrTriggerVals = np.array(snrTriggerVals)
+    
+    #print("snrTriggerTimes.shape: {}".format(snrTriggerTimes.shape))
+    
+    boolTriggerTimes = []
+    boolTriggerVals = []
+    with h5py.File(get_collect_path(), 'r') as f:
+        for highLevel in sorted(allowed.keys()):
+            for lowLevel in sorted(allowed[highLevel]):
+                bools = f[str(highLevel)][str(lowLevel)]['TimeSeries/p-valueTimeSeries/data'][:]
+                times = f[str(highLevel)][str(lowLevel)]['TimeSeries/p-valueTimeSeries/sample_times'][:]
+                times = boundaries(highLevel, lowLevel)[0] + 65.5 + times
+                #Do I want the high cutoff here or do I veto it later?
+                triggerIdx = np.where(bools > threshold_p)[0]
+                boolTriggerTimes = boolTriggerTimes + list(times[triggerIdx])
+                boolTriggerVals = boolTriggerVals + list(bools[triggerIdx])
+    
+    boolTriggerTimes = np.array(boolTriggerTimes)
+    boolTriggerVals = np.array(boolTriggerVals)
+    
+    #print("boolTriggerTimes.shape: {}".format(boolTriggerTimes.shape))
+    
+    triggerTimes = []
+    triggerSNR = []
+    triggerBool = []
+    
+    for i, snrT in enumerate(snrTriggerTimes):
+        idx = np.where(boolTriggerTimes == snrT)[0]
+        if len(idx) > 0:
+            triggerTimes.append(snrT)
+            triggerSNR.append(snrTriggerVals[i])
+            triggerBool.append(boolTriggerVals[idx[0]])
     
     triggerTimes = np.array(triggerTimes)
-    triggerVals = np.array(triggerVals)
+    triggerSNR = np.array(triggerSNR)
+    triggerBool = np.array(triggerBool)
+    
+    #print(triggerTimes.shape)
     
     #Cluster the triggers, use the symmetricInjectionWindow as a time
     #during which the triggers belong to the same event
-    clusters = clusterTriggerTimes(triggerTimes, time_span=1.0)
+    clusters = clusterTriggerTimes(triggerTimes, time_span=clusterTime)
     
     #Find the actual trigger times and veto thos with too high values
     finalTimes = []
-    finalVals = []
+    finalSNR = []
+    finalBool = []
     for r in clusters:
-        curr_cluster = triggerVals[r[0]:r[1]]
+        if mode.lower() == 'snr':
+            curr_cluster = triggerSNR[r[0]:r[1]]
+        elif mode.lower() == 'p-score':
+            curr_cluster = triggerBool[r[0]:r[1]]
         allowed_indices = np.where(curr_cluster < highCutoff)[0]
         try:
             #If no point is below the upper threshold, this part will fail
             max_allowed_val = max(curr_cluster[allowed_indices])
             max_idx = np.where(curr_cluster == max_allowed_val)[0][0]
             finalTimes.append(triggerTimes[r[0] + max_idx])
-            finalVals.append(triggerVals[r[0] + max_idx])
+            finalSNR.append(triggerSNR[r[0] + max_idx])
+            finalBool.append(triggerBool[r[0] + max_idx])
         except:
             pass
     
     #Find if trigger corresponds to actual injection
-    for trig in finalTimes:
-        found = False
-        min_distance = np.inf
-        for inj in injectionTimes:
-            if abs(trig - inj) <= symmetricInjectionWindow:
-                truePos.append((inj, trig))
-                found = True
-            else:
-                if abs(trig - inj) < abs(min_distance):
-                    min_distance = inj - trig
-        if not found:
-            falsePos.append((trig, min_distance))
+    for trigIdx, trig in enumerate(finalTimes):
+        tmp = np.abs(trig - injectionTimes)
+        min_idx = np.argmin(tmp)
+        if tmp[min_idx] <= symmetricInjectionWindow:
+            truePos.append((injectionTimes[min_idx], trig, finalSNR[trigIdx], finalBool[trigIdx]))
+        else:
+            falsePos.append((trig, injectionTimes[min_idx] - trig, finalSNR[trigIdx], finalBool[trigIdx]))
     
     #Find missed injections
     foundInj = [pt[0] for pt in truePos]
@@ -352,5 +437,196 @@ def getCombinedTriggersAtThreshold(threshold_snr, threshold_p, symmetricInjectio
             missed.append(inj)
     
     return (truePos, falsePos, missed)
+
+def writeStepToFile(g, x, mode):
+    truePos, falsePos, missed = getTriggersAtThreshold(x, mode=mode)
+    if mode.lower() == 'snr':
+        modeName = g.create_group('SNR')
+    elif mode.lower() == 'p-score':
+        modeName = g.create_group('p-score')
+    modeTrue = modeName.create_group('TruePositives')
+    modeFalse = modeName.create_group('FalsePositives')
     
+    modeName.create_dataset('Missed', data=np.array(missed))
+    modeTrue.create_dataset('InjectionTime', data=np.array([pt[0] for pt in truePos]))
+    modeTrue.create_dataset('RecoveredTime', data=np.array([pt[1] for pt in truePos]))
+    modeTrue.create_dataset('Value', data=np.array([pt[2] for pt in truePos]))
+    modeFalse.create_dataset('RecoveredTime', data=np.array([pt[0] for pt in falsePos]))
+    modeFalse.create_dataset('MinimalDistance', data=np.array([pt[1] for pt in falsePos]))
+    modeFalse.create_dataset('Value', data=np.array([pt[2] for pt in falsePos]))
+
+def calculateStepwise(steps, fileName, mode):
+    file_path = os.path.join(get_results_path(), fileName)
+    bar = progress_tracker(len(steps), name='Calculating')
+    with h5py.File(file_path, 'w') as f:
+        f.create_dataset('x', data=steps)
+        for x in steps:
+            x_group = f.create_group(str(x))
+            writeStepToFile(x_group, x, mode)
+            bar.iterate()
+    return
+
+def getFalseAlarmRate(stepFile, mode='snr', plot=True):
+    stepFile = os.path.join(get_results_path(), stepFile)
+    if mode.lower() == 'snr':
+        group_name = 'SNR'
+    elif mode.lower() == 'p-score':
+        group_name = 'p-score'
+    else:
+        raise NotImplementedError('No mode called {}'.format(mode))
     
+    with h5py.File(stepFile, 'r') as f:
+        x = f['x'][:]
+        y = np.array([len(f[str(i)][group_name]['FalsePositives/RecoveredTime']) for i in x])
+    
+    available = availableData()
+    
+    num_available = sum([len(available[k]) for k in available.keys()])
+    
+    observation_time = num_available * 4096.
+    
+    seconds_per_month = 60 * 60 * 24 * 30
+    
+    y = y / observation_time * seconds_per_month
+    
+    #print(seconds_per_month / observation_time)
+    
+    false_alarm_file = os.path.splitext(stepFile)[0] + '_false_alarm.hf5'
+    
+    with h5py.File(false_alarm_file, 'w') as f:
+        f.create_dataset('x', data=x)
+        f.create_dataset('y', data=y)
+    
+    if plot:
+        import matplotlib.pyplot as plt
+        plt.semilogy(x, y)
+        plt.grid()
+        plt.savefig(os.path.splitext(false_alarm_file)[0] + '.png')
+        plt.show()
+
+def getSensitivity(stepFile, falseAlarmFile, mode='snr', plot=True):
+    stepFile = os.path.join(get_results_path(), stepFile)
+    falseAlarmFile = os.path.join(get_results_path(), falseAlarmFile)
+    if mode.lower() == 'snr':
+        group_name = 'SNR'
+    elif mode.lower() == 'p-score':
+        group_name = 'p-score'
+    else:
+        raise NotImplementedError('No mode called {}'.format(mode))
+    
+    with h5py.File(falseAlarmFile, 'r') as f:
+        fa = f['y'][:]
+    
+    with h5py.File(stepFile, 'r') as f:
+        x = f['x'][:]
+        injTimesFound = [f[str(i)][group_name]['TruePositives/RecoveredTime'][:] for i in x]
+        injTimesMissed = [f[str(i)][group_name]['Missed'][:] for i in x]
+    
+    with h5py.File(get_stats_path(), 'r') as f:
+        allInjTimes = f['times'][:]
+        allInjDist = f['dist'][:]
+        allInjM1 = f['mass1'][:]
+        allInjM2 = f['mass2'][:]
+    
+    injDistFound = []
+    injM1Found = []
+    injM2Found = []
+    for injFound in injTimesFound:
+        tmpDist = []
+        tmpM1 = []
+        tmpM2 = []
+        for inj in injFound:
+            idx = np.where(allInjTimes == inj)[0][0]
+            tmpDist.append(allInjDist[idx])
+            tmpM1.append(allInjM1[idx])
+            tmpM2.append(allInjM2[idx])
+        injDistFound.append(tmpDist)
+        injM1Found.append(tmpM1)
+        injM2Found.append(tmpM2)
+    injDistMissed = []
+    injM1Missed = []
+    injM2Missed = []
+    for injMissed in injTimesMissed:
+        tmpDist = []
+        tmpM1 = []
+        tmpM2 = []
+        for inj in injMissed:
+            idx = np.where(allInjTimes == inj)[0][0]
+            tmpDist.append(allInjDist[idx])
+            tmpM1.append(allInjM1[idx])
+            tmpM2.append(allInjM2[idx])
+        injDistMissed.append(tmpDist)
+        injM1Missed.append(tmpM1)
+        injM2Missed.append(tmpM2)
+    
+    injMchirpFound = []
+    for i in range(len(injM1Found)):
+        tmpMchirp = []
+        for j in range(len(injM1Found[i])):
+            tmpMchirp.append(mchirp(injM1Found[i][j], injM2Found[i][j]))
+        injMchirpFound.append(tmpMchirp)
+    
+    injMchirpMissed = []
+    for i in range(len(injM1Missed)):
+        tmpMchirp = []
+        for j in range(len(injM1Missed[i])):
+            tmpMchirp.append(mchirp(injM1Missed[i][j], injM2Missed[i][j]))
+        injMchirpMissed.append(tmpMchirp)
+    
+    y = [volume_montecarlo(injDistFound[i], injDistMissed[i], injMchirpFound[i], injMchirpMissed[i], 'distance', 'volume', 'distance')]
+    
+    err = [pt[1] for pt in y]
+    err_rad = [(3 * pt / (4 * np.pi))**(1. / 3.) for pt in err]
+    vol = [pt[0] for pt in y]
+    rad = [(3 * pt / (4 * np.pi))**(1. / 3.) for pt in vol]
+    
+    sensitivity_file = os.path.splitext(stepFile)[0] + '_sensitivty.hf5'
+    
+    with h5py.File(sensitivity_file, 'w') as f:
+        f.create_dataset('x', data=fa)
+        f.create_dataset('error_volume', data=np.array(err))
+        f.create_dataset('error_radius', data=np.array(err_rad))
+        f.create_dataset('volume', data=np.array(vol))
+        f.create_dataset('radius', data=np.array(rad))
+    
+    if plot:
+        import matplotlib.pyplot as plt
+        plt.semilogx(np.flip(fa), np.array(rad))
+        plt.grid()
+        plt.savefig(os.path.splitext(sensitivity_file)[0] + '.png')
+        plt.xlabel('False alarms per 30 days')
+        plt.ylabel('Radius of sensitive sphere')
+        plt.show()
+
+def writeCombinedStepToFile(g, x, y, mode):
+    truePos, falsePos, missed = getCombinedTriggersAtThreshold(x, y, mode=mode)
+    modeTrue = g.create_group('TruePositives')
+    modeFalse = g.create_group('FalsePositives')
+    
+    g.create_dataset('Missed', data=np.array(missed))
+    
+    modeTrue.create_dataset('InjectionTime', data=np.array([pt[0] for pt in truePos]))
+    modeTrue.create_dataset('RecoveredTime', data=np.array([pt[1] for pt in truePos]))
+    modeTrue.create_dataset('RecoveredSNR', data=np.array([pt[2] for pt in truePos]))
+    modeTrue.create_dataset('RecoveredP-score', data=np.array([pt[3] for pt in truePos]))
+    
+    modeFalse.create_dataset('RecoveredTime', data=np.array([pt[0] for pt in falsePos]))                                                        
+    modeFalse.create_dataset('MinimalDistance', data=np.array([pt[1] for pt in falsePos]))
+    modeFalse.create_dataset('RecoveredSNR', data=np.array([pt[2] for pt in falsePos]))
+    modeFalse.create_dataset('RecoveredP-score', data=np.array([pt[3] for pt in falsePos]))
+    return
+
+def calculateStepwiseCombined(snrSteps, pSteps, fileName, mode):
+    file_path = os.path.join(get_results_path(), fileName)
+    bar = progress_tracker(len(snrSteps) * len(pSteps), name='Calculating steps')
+    with h5py.File(file_path, 'w') as f:
+        f.create_dataset('snr', data=snrSteps)
+        f.create_dataset('p-score', data=pSteps)
+        f.create_dataset('mode', data=np.str(mode))
+        for x in snrSteps:
+            x_group = f.create_group(str(x))
+            for y in pSteps:
+                y_group = x_group.create_group(str(y))
+                writeCombinedStepToFile(y_group, x, y, mode)
+                bar.iterate()
+    return
